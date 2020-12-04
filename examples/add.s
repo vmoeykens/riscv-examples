5b2eca7c6debab546b03a83631eb1a0d801c9f81
vmoeykens <vincent@beta.team>
2020-12-04 17:43:31 -0500

    Updates add.s example

--- examples/add.s
+++ examples/add.s
@@ -1,1 +1,8 @@
-ADDI x10,x0,10 # Add Immediate (Constant) of value 10 to register x0 (zero constant) and store it in register x10 (a0)
+main: 
+   ADDI x10,x0,10 # Add Immediate (Constant) of value 10 to register x0 (zero constant) and store it in register x10 (a0)
+   ADDI x11,x0,5 # Add Immediate 10 to x0 and store in x11
+   ADD x12,x10,x11 # Add register x10 and x11 and store in x12
+   ADDI x13,x12,207 # Add register x12 and immediate 207 and store in register x13
+main
+exit:
+
